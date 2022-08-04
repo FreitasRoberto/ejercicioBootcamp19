d287cbf2864286fa2981143d060e30d1d09e17ad
Roberto Freitas <freitasroberto04@gmail.com>
2022-08-04 21:47:12 +0200

    Add files via upload

---
+++ ejercicioBootcamp19/const lista = [.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Roberto",
+    28,
+    false,
+    new Date(1993, 10, 04),
+    {
+        titulo: "El triunfo del dinero",
+        autor: "Niall Ferguson",
+        fecha: new Date(2008, 10, 13),
+        url: "https://www.amazon.es/El-triunfo-del-dinero-finanzas/dp/8483068133"
+    },
+];
